db953d6f68b000259a038423d03caae041c26c5f
samir-bueno <samirbueno585@gmail.com>
2024-09-16 11:20:51 -0300

    termine los test para la verificaion de los metodos de la clase apuesta

--- test/test_apuesta.py
+++ test/test_apuesta.py
@@ -39,3 +39,43 @@ def test_tomarFicha_valor_sin_argumentos():
     a.tomarFicha()
     assert(a.fichas == 4)
 
+
+def test_tomarFichaError():
+    with pytest.raises(ValueError):
+        a = Apuesta()
+        a.fichas = 5
+        a.tomarFicha(6)
+    
+    a = Apuesta()
+    a.fichas = 5
+    a.tomarFicha(5)
+    assert(a.fichas==0)
+
+def test_tomarTodasfichas():
+    a = Apuesta()
+
+    a.ponerFicha(1)
+    a.ponerFicha(2)
+
+    assert(a.fichas==a.fichas)
+
+def test_tieneFichas():
+    a = Apuesta()
+
+    a.ponerFicha(1)
+    assert(a.fichas>0)
+
+def test_estaVacio():
+    a = Apuesta()
+
+    a.ponerFicha(1)
+    a.tomarFicha(1)
+
+    assert(a.fichas==0)
+
+
+
+
+
+
+
