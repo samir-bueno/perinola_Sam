160387d83839bd3dd01a950411e51c232b19c230
samir-bueno <samirbueno585@gmail.com>
2024-09-16 11:17:08 -0300

    Agregue a la clase apuesta los metodos que me faltaban tomarTodas, tieneFicha y estaVacio

--- clase_apuestas.py
+++ clase_apuestas.py
@@ -1,11 +1,31 @@
 class Apuesta:
     def __init__(self):
         self.fichas = 0
+
     def __repr__(self):
         return f"apuesta {self.fichas} fichas"
+    
     def ponerFicha(self, cuantas=1): 
         self.fichas = self.fichas + cuantas 
+
     def tomarFicha(self, cuantas=1):
         if cuantas > self.fichas:
             raise ValueError(f"No hay tantas fichas(quedan{self.fichas})")
-        self.fichas = self.fichas - cuantas 
+        self.fichas = self.fichas - cuantas
+
+    def tomarTodas(self):       
+        return self.fichas
+    
+    def tieneFichas(self,cuantas=1):
+        return self.fichas >= cuantas
+    
+    def estaVacio(self):
+        return self.fichas == 0
+    
+
+        
+    
+
+    
+    
+
